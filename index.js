cae6e6431613a8efbfd86eaf4e222db96873cbfd
Pedro Victor <pedro@fflank.org>
2022-11-23 13:28:21 -0300

    Print data in txt file

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const request = require("request");
 const cheerio = require("cheerio");
+const fs = require("fs");
 
-let url = "http://imdb.com/chart/moviemeter"
+let url = "http://imdb.com/chart/moviemeter";
 
 async function getData() {
-  request(url, function(err, res, body){
-  if(err) console.log(`Error - ${err}`);
+  request(url, function (err, res, body) {
+    if (err) console.log(`Error - ${err}`);
 
-  const $ = cheerio.load(body);
+    const $ = cheerio.load(body);
 
-  $('.lister-list tr').each(function() {
-    const title = $(this).find('.titleColumn a').text().trim();
-    const rating = $(this).find('.ratingColumn strong').text().trim();
+    $(".lister-list tr").each(function () {
+      const title = $(this).find(".titleColumn a").text().trim();
+      const rating = $(this).find(".ratingColumn strong").text().trim();
 
-    console.log(`${title} - ${rating}\n`);
+      console.log(`${title} - ${rating}\n`);
+
+      fs.appendFile("list.txt", `${title} - ${rating}\n`, (err) => {
+        return err;
+      });
+    });
   });
-});
-};
+}
 
 getData();
